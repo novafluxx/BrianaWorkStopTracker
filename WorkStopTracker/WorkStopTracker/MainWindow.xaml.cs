cc2695146c2b57779f2e9cf52115860e795332fd
Justin High <novafluxx@gmail.com>
2019-01-14 23:32:05 -0500

    Beta

--- WorkStopTracker/WorkStopTracker/MainWindow.xaml.cs
+++ WorkStopTracker/WorkStopTracker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,53 @@ namespace WorkStopTracker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private DateTime startTime = new DateTime();
+        private string filename = @"timelogfile.csv";
+
+        private async void StopLogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UnicodeEncoding uniencoding = new UnicodeEncoding();
+
+            var endTime = DateTime.Now;
+
+            TimeSpan span = endTime.Subtract(startTime);
+
+            var stringToWrite = $"{startTime.ToString()},{endTime.ToString()},{span.Seconds}\n";
+
+            byte[] bytesToWrite = uniencoding.GetBytes(stringToWrite);
+
+            using (FileStream fs = File.Open(filename, FileMode.OpenOrCreate))
+            {
+                fs.Seek(0, SeekOrigin.End);
+                await fs.WriteAsync(bytesToWrite, 0, bytesToWrite.Length);
+            }
+
+            StopLogBtn.IsEnabled = false;
+            startLogBtn.IsEnabled = true;
+        }
+
+        private async void StartLogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if the log file already exists.
+            // If it does not create it and write the headers.
+            if (!File.Exists(filename))
+            {
+                UnicodeEncoding uniencoding = new UnicodeEncoding();
+                var stringToWrite = $"Start Time,Stop Time,Time Spent (Seconds)\n";
+
+                byte[] bytesToWrite = uniencoding.GetBytes(stringToWrite);
+
+                using (FileStream fs = File.Open(@"timelogfile.csv", FileMode.OpenOrCreate))
+                {
+                    await fs.WriteAsync(bytesToWrite, 0, bytesToWrite.Length);
+                }
+            }
+
+            startTime = DateTime.Now;
+            startLogBtn.IsEnabled = false;
+            StopLogBtn.IsEnabled = true;
         }
     }
 }
